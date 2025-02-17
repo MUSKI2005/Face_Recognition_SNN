83955dd5fa908388d722cf7313961665dfedfacb
MUSKI2005 <23.muskansharmaudr@gmail.com>
2025-02-17 21:38:27 +0530

    New changes made regarding branching

--- App_SNN.py
+++ App_SNN.py
@@ -1,2 +1,2 @@
-# Here the code for app will be written here \
+# Here the code for app will be written here 
 # We'ld be using 4 different kinds of widgets that are often being used in the app developement.
