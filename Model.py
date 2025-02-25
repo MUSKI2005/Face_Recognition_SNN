1b8a0493fd123145c9a30706f72a0ae5f9b4cbb4
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-02-25 23:30:10 +0530

    Declaring the similarity function, which is going to return absolute value.

--- Model.py
+++ Model.py
@@ -343,6 +343,9 @@ class L1Dist(Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+# magic happens here -similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding) #this gonna return the absolute value
 
 
 
