b9384391dc0f06b848ec3b1cefa7ab366bc77da0
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-03-09 23:43:59 +0530

    Disabling the checkpoints

--- Model.py
+++ Model.py
@@ -496,7 +496,7 @@ def train(data,val_data,EPOCHS):
 
         #    Disabling checkpoint saving for quick testing
         if epoch% 10==0: #save checkpoints at every 10 epochs
-
+           checkpoint.save(file_prefix=checkpoint_prefix)
 
 
 
