673b0fa7f1c231d993d0bb32ee79c4f74ab68d18
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-03-11 23:43:01 +0530

    Checking whether we enumerated right or wrong

--- Model.py
+++ Model.py
@@ -535,7 +535,7 @@ print("This is the array of the labels in test_var:", test_var[2])
 
 # this also does means that our test_input=test_var[0], test_val=test_var[1], 
 
-
+# now checking whether we enemurated right or not
 
 
 
