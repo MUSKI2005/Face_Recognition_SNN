d600cbb51fa653180e19227c744ffea04862ea18
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-03-16 23:43:39 +0530

    Config experimental to fetch devices

--- Model.py
+++ Model.py
@@ -569,6 +569,8 @@ siamese_network.summary()
 
 #  Ensure that TensorFlow is using GPU if available
 physical_devices = tf.config.list_physical_devices('GPU')
+if physical_devices:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 
