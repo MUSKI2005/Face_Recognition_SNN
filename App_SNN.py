62092bac204c9b366171c015fadb28f21132ae21
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-03-25 22:47:47 +0530

    importing other dependencies

--- App_SNN.py
+++ App_SNN.py
@@ -5,12 +5,12 @@
 from kivy.app import App
 from kivy.uix.boxLayout import BoxLayout
 
-# import kivy UX components
+# Import kivy UX components
 from kivy.uix.image import Image 
-from kivy.graphics.texture import Texture
-from kivy.logger import Logger
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
-
+# Import oth kivy dependencies
 
 
 
