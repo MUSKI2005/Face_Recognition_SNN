846038239b2862dca4505c671466689858e22a89
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-03-25 22:37:28 +0530

    Importing kivy dependencies for the app

--- Model.py
+++ Model.py
@@ -746,6 +746,7 @@ print(np.squeeze(results)>0.5)
 
 # now the no. of images passing the verification
 print(np.squeeze(results)>0.7) #can even set this high ,let's say either 0.6,.7,.8 checking
+print(np.squeeze(results)>0.8)
 
 
 
