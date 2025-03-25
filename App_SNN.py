0dcc15b6ea34ce769f939b04690cc7ae8127406b
MUSKAN SHARMA <23.muskansharmaudr@gmail.com>
2025-03-25 22:44:29 +0530

    importing kivy dependencies

--- App_SNN.py
+++ App_SNN.py
@@ -1,2 +1,39 @@
 # Here the code for app will be written here 
 # We'ld be using 4 different kinds of widgets that are often being used in the app developement.
+
+# importing kivy dependencies
+from kivy.app import App
+from kivy.uix.boxLayout import BoxLayout
+
+# # import kivy UX components
+# from kivy.uix.image import Image 
+# from kivy.graphics.texture import Texture
+# from kivy.logger import Logger
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
